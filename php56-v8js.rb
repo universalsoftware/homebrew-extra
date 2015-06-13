7bef2904c1ee681dd32f8eed181d1339668a093f
Artem Butusov <artem.butusov@unvsoft.com>
2015-06-12 20:32:00 -0400

    php56-v8js

---
+++ php56-v8js.rb
@@ -0,0 +1,25 @@
+require File.join(File.dirname(__FILE__), '../../homebrew/homebrew-php/Abstract/abstract-php-extension')
+
+class Php56V8js < AbstractPhp56Extension
+  init
+  homepage 'https://github.com/preillyme/v8js'
+  url 'https://github.com/preillyme/v8js.git', :revision => 'e67f1f4c9cde651706d8474a3d43f2d437a93ca8'
+  version '20150501'
+  head 'https://github.com/preillyme/v8js.git'
+
+  depends_on 'v8'
+
+  def install
+    inreplace "config.m4", "-Wl,--rpath=", "-L"
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--with-v8js=#{Formula['v8'].opt_prefix}"
+    system "make", "CXXFLAGS=-Wno-narrowing"
+    prefix.install "modules/v8js.so"
+    write_config_file if build.with? "config-file"
+  end
+end
