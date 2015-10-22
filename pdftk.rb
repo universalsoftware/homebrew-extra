3a279c09ece650d78f8753b368337dc3293bddff
Artem Butusov <artem.butusov@unvsoft.com>
2015-10-22 00:36:47 -0400

    Migrate formulas from sormy/homebrew-extra repo + new pdftk formula (linked with gcc49 instead of buggy gcc5)

---
+++ pdftk.rb
@@ -0,0 +1,46 @@
+require 'formula'
+
+class Pdftk < Formula
+  homepage 'https://github.com/docmunch/pdftk'
+  url 'https://github.com/docmunch/pdftk.git', :tag => "v2.02"
+
+  head 'https://github.com/docmunch/pdftk.git', :branch => 'master'
+
+  option "with-java", "Build gcc using --with-java instead of --with-all-languages"
+
+  # GCC 5 doesn't work (gcj hangs) / 2015-10-22
+  depends_on 'ecj'
+  depends_on 'gcc49' => ['with-java'] if build.with?("java")
+  depends_on 'gcc49' => ['with-all-languages'] if !build.with?("java")
+
+  def patches
+    'https://raw.githubusercontent.com/docmunch/homebrew-pdftk/master/patch.diff'
+  end
+
+  def install
+    # Disable check on libstdc++ since we're using gcc and not clang.
+    cxxstdlib_check :skip
+
+    doc.mkpath
+    doc.install 'changelog.html', 'changelog.txt', 'pdftk.1.html', 'pdftk.1.txt', 'license_gpl_pdftk'
+    man1.install 'pdftk.1'
+
+    cd 'pdftk' do
+      gcc = Formula['gcc49']
+      inreplace 'Makefile.OSX-10.6' do |s|
+        s.gsub! '@HOMEBREW_PREFIX@', HOMEBREW_PREFIX
+        s.gsub! '@PREFIX@', gcc.prefix
+        s.gsub! '@GCC_VERSION@', gcc.version
+        s.gsub! '@GCC_VERSION_SUFFIX@', gcc.version_suffix
+      end
+
+      system 'make', '-f', 'Makefile.OSX-10.6'
+    end
+
+    bin.install 'pdftk/pdftk'
+  end
+
+  test do
+    system bin/'pdftk', '--version'
+  end
+end
