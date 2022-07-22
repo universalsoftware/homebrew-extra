3a92e03b12c4ffb46a3026eeed0f138ec2da289d
Vadim Safonov <vadim@emsow.com>
2022-07-22 18:16:05 +0300

    remove old

--- gdcm.rb
+++ gdcm.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gdcm < Formula
   homepage 'http://gdcm.sourceforge.net'
-  url 'http://downloads.sourceforge.net/project/gdcm/gdcm%202.x/GDCM%202.4.4/gdcm-2.4.4.tar.bz2'
-  sha1 '4b77a857cf8432da72140a5e7d20f78c091ee019'
+  url 'http://downloads.sourceforge.net/project/gdcm/gdcm%202.x/GDCM%202.4.6/gdcm-2.4.6.tar.bz2'
+  sha1 'd3699a5995635fdc3db7cab7f8614c5b90db1a433f84a12e048d3eece57cf865'
 
   depends_on 'cmake' => :build
 
