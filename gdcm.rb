c9f10987b7b7c806537d24fdd44c8f5a8c42a3cd
Vadim Safonov <vadim@emsow.com>
2022-07-22 18:16:53 +0300

    replace checksum

--- gdcm.rb
+++ gdcm.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Gdcm < Formula
   homepage 'http://gdcm.sourceforge.net'
   url 'http://downloads.sourceforge.net/project/gdcm/gdcm%202.x/GDCM%202.4.6/gdcm-2.4.6.tar.bz2'
-  sha1 'd3699a5995635fdc3db7cab7f8614c5b90db1a433f84a12e048d3eece57cf865'
+  sha256 'd3699a5995635fdc3db7cab7f8614c5b90db1a433f84a12e048d3eece57cf865'
 
   depends_on 'cmake' => :build
 
