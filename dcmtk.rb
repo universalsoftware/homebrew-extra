000742f23ff5e1aeb6446daab60949ca33e790e1
Vadim Safonov <vadim@emsow.com>
2022-07-22 18:59:53 +0300

    fix dcmtk

--- dcmtk.rb
+++ dcmtk.rb
@@ -1,44 +1,61 @@
 class Dcmtk < Formula
   desc "OFFIS DICOM toolkit command-line utilities"
-  homepage "http://dicom.offis.de/dcmtk.php.en"
+  homepage "https://dicom.offis.de/dcmtk.php.en"
+  head "https://git.dcmtk.org/dcmtk.git", branch: "master"
+
+  stable do
+    url "https://dicom.offis.de/download/dcmtk/dcmtk366/dcmtk-3.6.6.tar.gz"
+    sha256 "6859c62b290ee55677093cccfd6029c04186d91cf99c7642ae43627387f3458e"
+
+    # Fix build for Apple Silicon.
+    # Issue ref: https://support.dcmtk.org/redmine/issues/957
+    # TODO: Remove in the next release along with stable block
+    patch do
+      url "https://git.dcmtk.org/?p=dcmtk.git;a=patch;h=5fba853b6f7c13b02bed28bd9f7d3f450e4c72bb"
+      sha256 "40ca9e6f377951e2d24a509a6c95b9e572224d74d694f0d648b8b33e4d67e285"
+    end
+  end
+
+  livecheck do
+    url "https://dicom.offis.de/download/dcmtk/release/"
+    regex(/href=.*?dcmtk[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
   
-  # Current snapshot used for stable now.
-  url "https://github.com/DCMTK/dcmtk/archive/DCMTK-3.6.5+_20191213.tar.gz"
-  sha256 "53a51f176dd0792d54613979d79b399a38f2dfde3266dc7f1cf05bb216188697"
-  version "3.6.5-20191213"
-  
-  head "http://git.dcmtk.org/dcmtk.git"
-
-  option "with-docs", "Install development libraries/headers and HTML docs"
-  option "with-openssl", "Configure DCMTK with support for OpenSSL"
-  option "with-libiconv", "Build with brewed libiconv. Dcmtk and system libiconv can have problems with utf8."
-
-  depends_on "cmake" => :build
-  depends_on "doxygen" => :build if build.with? "docs"
-  depends_on "libpng"
-  depends_on "libtiff"
-  depends_on "openssl"
-  depends_on "homebrew/dupes/libiconv" => :optional
-
   patch do
     url "file://" + File.dirname(__FILE__) + "/patches/dcmtk-3.6.4.patch"
     sha256 "0a1b2fbf6396e170d58cfd81c81f6a631f68b8bb5ee5f5534cbda740b65f1b60"
   end
 
+  bottle do
+    rebuild 1
+    sha256 arm64_monterey: "2b707106032247e1d1832e918ec0e560b2e81af4ed0a1ee659f0803c4c172141"
+    sha256 arm64_big_sur:  "b101e7090c39b48225ab0d1d895236c849b613cf1f4e9b187c7818c6e9e7b78a"
+    sha256 monterey:       "abf4a42c7a34004707c70fb518e1a9fc11582e360a886a8c109b5ce6fcd6492a"
+    sha256 big_sur:        "847e85cc8b29b4caaf58cf0aa001b06082f2ea83bceaf6a0b5c90abc27b1f3bb"
+    sha256 catalina:       "f7872a94bd7c98fa785a61d7d9c9dd00e577002b9d563fc403f4913011457de1"
+    sha256 x86_64_linux:   "bea864995df958ccf4fb449d1347b686ed28171e9417845b7a768eee2b7eab6f"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "openssl@1.1"
+
+  uses_from_macos "libxml2"
+
   def install
-#    ENV.m64 if MacOS.prefer_64_bit?
+    system "cmake", "-S", ".", "-B", "build/shared", *std_cmake_args,
+                    "-DBUILD_SHARED_LIBS=ON",
+                    "-DCMAKE_INSTALL_RPATH=#{rpath}"
+    system "cmake", "--build", "build/shared"
+    system "cmake", "--install", "build/shared"
 
-    args = std_cmake_args
-    args << "-DDCMTK_WITH_DOXYGEN=YES" if build.with? "docs"
-    args << "-DDCMTK_WITH_OPENSSL=YES" if build.with? "openssl"
-    args << "-DDCMTK_WITH_ICONV=YES -DLIBICONV_DIR=#{Formula["libiconv"].opt_prefix}" if build.with? "libiconv"
-    args << ".."
+    system "cmake", "-S", ".", "-B", "build/static", *std_cmake_args,
+                    "-DBUILD_SHARED_LIBS=OFF"
+    system "cmake", "--build", "build/static"
+    lib.install Dir["build/static/lib/*.a"]
 
-    mkdir "build" do
-      system "cmake", *args
-      system "make", "DOXYGEN" if build.with? "docs"
-      system "make", "install"
-    end
+    inreplace lib/"cmake/dcmtk/DCMTKConfig.cmake", "#{Superenv.shims_path}/", ""
   end
 
   test do
